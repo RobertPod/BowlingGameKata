8d48b83d904f157e6083dd50c7130dd10e193f27
RobertPod <rob.podsiadly@gmail.com>
2020-01-29 22:02:45 +0100

    Done kata (almost)

--- src/main/java/org/bmc/tdd/BowlingGame.java
+++ src/main/java/org/bmc/tdd/BowlingGame.java
@@ -2,7 +2,48 @@ package org.bmc.tdd;
 
 public class BowlingGame {
 
-    public int getScore() {
-        return -1;
+  public int getScore(char[] input) {
+    int score = 0;
+    int multiplierNext = 1;
+    int multiplierNextNext = 1;
+    var len = input.length;
+    int round = 0;
+
+    for (int i = 0; i < len; i++) {
+      switch (input[i]) {
+        case '-':
+          multiplierNext = multiplierNextNext;
+          multiplierNextNext = 1;
+          break;
+
+        case '/':
+          score += multiplierNext * (10 - Integer.parseInt(Character.toString(input[i - 1])));
+          multiplierNext = 2;
+          break;
+
+        case 'X':
+          if (round<=20) {
+            score += multiplierNext * 10;
+          }
+          round++;
+
+//          multiplierNext++;
+          multiplierNext = multiplierNext == 3 ? multiplierNext : multiplierNext+1;
+          multiplierNextNext = multiplierNextNext == 3 ? multiplierNextNext : multiplierNextNext+1;
+          break;
+
+        default:
+          if (round<20) {
+            score += multiplierNext * Integer.parseInt(Character.toString(input[i]));
+          } else {
+
+            score += Integer.parseInt(Character.toString(input[i]));
+          }
+          multiplierNext = multiplierNextNext;
+          multiplierNextNext = 1;
+      }
+      round++;
     }
+    return score;
+  }
 }
--- src/test/java/org/bmc/tdd/BowlingGameTest.java
+++ src/test/java/org/bmc/tdd/BowlingGameTest.java
@@ -2,22 +2,75 @@ package org.bmc.tdd;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class BowlingGameTest {
 
-    BowlingGame theGame = new BowlingGame();
+  BowlingGame theGame = new BowlingGame();
 
-    @Test
-    public void shouldScoreOneWhenRolledOneWithJUnit() {
-        assertEquals(1, theGame.getScore());
-    }
+//  @Test
+//  public void shouldScoreOneWhenRolledOneWithJUnit() {
+//    assertEquals(1, theGame.getScore());
+//  }
 
-    @Test
-    public void shouldScoreOneWhenRolledOneWithAssertJ() {
-        Assertions.assertThat(theGame.getScore()).isEqualTo(1);
-    }
+  @ParameterizedTest
+  @CsvFileSource(resources = "/testdata.csv")
+  public void shouldScoreZero(String input, int expected) {
+    assertEquals(expected, theGame.getScore(input.toCharArray()));
+  }
+
+  @Test
+  public void shouldScoreZero() {
+    // given
+    char[] input = {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-',
+        '-', '-', '-', '-'};
+
+    assertEquals(0, theGame.getScore(input));
+  }
+
+  @Test
+  public void shouldScoreOne() {
+    // given
+    char[] input = {'1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-',
+        '-', '-', '-', '-'};
+
+    assertEquals(1, theGame.getScore(input));
+  }
+
+  @Test
+  public void shouldSpareAndScoreOneAndSpare() {
+    // given
+    char[] input = {'1', '/', '1', '-', '-', '-', '-', '-', '-', '-',
+                    '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
+    assertEquals(12, theGame.getScore(input));
+  }
+
+  @Test
+  public void shouldScoreOneAndSpare() {
+    // given
+    char[] input = {'1', '/', '1', '-', '1', '-', '-', '-', '-', '-',
+        '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
+    assertEquals(13, theGame.getScore(input));
+  }
+
+  @Test
+  public void shouldScoreOneAndSpareInNextRound() {
+    // given
+    char[] input = {'-', '1', 'X', '1', '1', '-', '-', '-', '-',
+        '-', '-', '-', '-', '-', '-', '-', '-', '-', '1'};
+
+    assertEquals(16, theGame.getScore(input));
+  }
+
+//  @Test
+//  public void shouldScoreOneWhenRolledOneWithAssertJ() {
+//    Assertions.assertThat(theGame.getScore()).isEqualTo(1);
+//  }
 
 }
